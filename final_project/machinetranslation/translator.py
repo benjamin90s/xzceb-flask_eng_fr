d3b57005dbd2996030e9ef59d280d6e33b0b10ae
benjamin90s <debbarmabenjamin69@gmail.com>
2022-04-02 14:28:42 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,41 +1,33 @@
-import json
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+"""import libraries"""
 import os
+from ibm_watson import LanguageTranslatorV3 #from IBM
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator#authenticator
 from dotenv import load_dotenv
 
 load_dotenv()
-
+"""assigns api & url."""
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator('Vbm9RmMIYQW4r_KfynvnF5fNw6gGbKQcWNGcnPLtlNcn')
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+"""Watson language 
+translator URL"""
 
-@app.route("/englishToFrench")
-def english_to_french():
-    textToTranslate = request.args.get('textToTranslate')
-    english_text= translator.english_to_french(textToTranslate)
-    return english_text
+language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/21f4fb62-358e-4191-afd7-0680eaa63285')
 
-@app.route("/")
-def renderIndexPage():
-    return render_template('index.html')
-
-    @app.route("/frenchToEnglish")
-def french_to_english():
-    textToTranslate = request.args.get('textToTranslate')
-    french_text= translator.french_to_english(textToTranslate)
+def english_to_french(english_text):
+    """function to translate eng to french"""
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
     return french_text
 
-@app.route("/")
-def renderIndexPage():
-    return render_template('index.html')
+def french_to_english(french_text):
+    """function to translate french to eng"""
+    translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
